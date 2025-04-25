14dc349188b9e18cccf1bfedce5de205d7795e83
Abhirrami <abhirramims2005@gmail.com>
2025-04-25 13:56:19 +0530

    Delete client directory

--- client/src/services/authDonorService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const API = 'http://localhost:5000/api/donor';
-
-export const signupDonor = async(data) => await axios.post(`${API}/signup`, data).then(res => res.data); 
-export const loginDonor = async(data) => await axios.post(`${API}/login`, data).then(res => res.data);
