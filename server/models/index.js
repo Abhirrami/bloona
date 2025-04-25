639a911f107e104e9d9cc899e78692bc282f5d5c
Abhirrami <abhirramims2005@gmail.com>
2025-04-25 13:56:53 +0530

    Delete server directory

--- server/models/index.js
+++
@@ -1,9 +0,0 @@
-const sequelize = require("../config/database");
-const Donor = require("./Donor");
-const Hospital = require("./Hospital");
-
-const syncModels = async () => {
-  await sequelize.sync({ alter: true });
-};
-
-module.exports = { sequelize, Donor, Hospital, syncModels };
--- server/routes/authDonorRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { donorSignup, donorLogin } = require("../controllers/authDonorConroller");
-
-router.post("/signup", donorSignup);
-router.post("/login", donorLogin);
-
-module.exports = router;
